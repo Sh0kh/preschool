8279bbdb10a8c3138be9f52466413a3d680c9c32
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-11-07 11:53:10 +0500

    edit

--- src/Components/Groups/_components/Delete.jsx
+++ src/Components/Groups/_components/Delete.jsx
@@ -7,7 +7,7 @@ import {
     DialogFooter,
     Typography,
 } from "@material-tailwind/react";
-import { Alert } from "../../../../../motor/src/utils/Alert";
+import { Alert } from "../../../utils/Alert";
 import DeleteIcon from "../../UI/Icons/DeleteIcon";
 import { GroupsApi } from "../../../utils/Controllers/GroupsApi";
 
